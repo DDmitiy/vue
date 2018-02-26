11d5c421cdd5a03d29d72d916f7ce804c237ac15
ddmitiy <dudov.dm@gmail.com>
2018-02-26 15:28:05 +0300

    transfer static code to vue-cli

---
+++ new-proj/src/router/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import home from '@/components/home'
+import test from '@/components/test'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+        path: '/',
+        name: 'HelloWorld',
+        component: HelloWorld
+    },
+    {
+     path: '/home',
+     component: home
+    },
+    {
+        path: '/test',
+        component: test
+    }
+  ]
+})
